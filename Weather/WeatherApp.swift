8f7ec5d89c20221a73b37202cd98dacf4daa7987
tipachel88 <tipakoroche@gmail.com>
2022-03-01 13:02:32 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Tipachel on 01.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
